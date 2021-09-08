cfbbcccd92dadf5fb554cbf5d2326c33883ce24c
Trainraider <trainraider7@gmail.com>
2021-09-07 23:17:59 -0400

    updated license header

--- include/lib.h
+++ include/lib.h
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: GPL-2.0-only
+// Full license at https://spdx.org/licenses/GPL-2.0.html
+
 #include "types.h"
 
 VM say_hello(VM);
--- tests/say_hello.c
+++ tests/say_hello.c
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: GPL-2.0-only
+// Full license at https://spdx.org/licenses/GPL-2.0.html
+
 #include "lib.h"
 
 IM main(VM)
